c1987686eafa1787ff44e14f8321dc269b7cabdc
bhilaremahesh205 <bhilaremahesh205@gmail.com>
2024-10-07 19:34:00 +0530

    Initial commit

---
+++ src/main/java/com/springSecurityDemo/SpringSecurityDemo/controller/home.java
@@ -0,0 +1,28 @@
+package com.springSecurityDemo.SpringSecurityDemo.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/home")
+public class home {
+    @PreAuthorize("hasRole('NORMAL')")
+    @GetMapping("/normal")
+    public ResponseEntity<String> normalUser(){
+        return ResponseEntity.ok("Yes , i am normal user");
+    }
+
+    @PreAuthorize("hasRole('ADMIN')")
+    @GetMapping("/admin")
+    public ResponseEntity<String> adminUser(){
+        return ResponseEntity.ok("Yes , i am admin user");
+    }
+
+    @GetMapping("/public")
+    public ResponseEntity<String> publicUser(){
+        return ResponseEntity.ok("Yes , i am public user");
+    }
+}
